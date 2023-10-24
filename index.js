6b492512ab4084c810b098aa7824a9abfda797b1
parsa <parsaddbb@gmail.com>
2023-10-24 11:19:29 +0330

    fix env file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const app  = express();
 
@@ -14,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
 
-mongoose.connect("mongodb+srv://parsa:4vMhhaVe1klHj9be@cluster0.6ngxzut.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{debug("connected to database")})
 .catch(()=>{debug("could not connect")})
 
@@ -24,6 +26,7 @@ const PORT  = process.env.PORT || 3000 ;
 
 app.listen(PORT , ()=>{console.log(`lestening to port ${PORT}`)})
 
+require("dotenv").config()
 
 
 
